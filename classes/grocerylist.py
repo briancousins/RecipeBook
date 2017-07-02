b20a3e64cd669e343ca99167c0901fe9d74edd2b
Brian Cousins <briancousins@users.noreply.github.com>
2017-07-01 21:08:28 -0400

    finished main_ui_sort_list and changes that required. I didn't go back to re-test main.py but it should still work (haha should) (#2)

--- classes/grocerylist.py
+++ classes/grocerylist.py
@@ -5,13 +5,16 @@ from operator import itemgetter
 # category an ingredient is part of
 class groceryList:
 
+    #setup and populate the grocery_list[] (wunderlist_item_id, category_order, item_name)
     def __init__(self, wunderlist_list_id, wunderpy_wrapper):
         self.wunderlist_list_id = wunderlist_list_id
         self.wp = wunderpy_wrapper
 
         self.grocery_list = [] # (wunderlist_item_id, category_order, item_name)
         self.wunderlist_tasks = []
-        # self.wunderlist_order_obj  = []
+
+        #populate self.grocery_list
+        self.get_tasks()
 
     def get_tasks(self):
         self.wunderlist_tasks = self.wp.get_tasks(self.wunderlist_list_id)
@@ -22,16 +25,44 @@ class groceryList:
         for element in self.wunderlist_tasks:
             self.grocery_list.append([element['id'], None, element['title']])
 
-    # grabs categories (actually their order via implicit argument to groceryStore) by item name
-    def get_category_for_element(self, list_element, grocery_store):
-        return grocery_store.get_and_fix_category_for_ingredient(list_element[2])
+    # grabs category for an element (actually their order, via implicit argument to grocery_store) by item name
+    def get_and_fix_category_for_element(self, element, grocery_store):
+        return grocery_store.get_and_fix_category_for_ingredient(element[2])
 
+    def get_category_for_element(self, element, grocery_store):
+        category_id_col = 2
+
+        category = grocery_store.get_category_for_ingredient(element[2])
+
+        if category is None:
+            cat_id = None
+        else:
+            cat_id = category[category_id_col]
+
+        return cat_id
+
+    #gets the categories for each element in grocery list via get_category_for_element
     def get_categories(self, grocery_store):
-        for index, list_element in enumerate(self.grocery_list):
-            self.grocery_list[index][1] = self.get_category_for_element(list_element, grocery_store)
+        for index, element in enumerate(self.grocery_list):
+            self.grocery_list[index][1] = self.get_category_for_element(element, grocery_store)
 
+    def set_category_for_item(self,item,category):
+        for index, element in enumerate(self.grocery_list):
+            if element[2] == item:
+                self.grocery_list[index][1] = category
+
+
+    #order self.grocery_list based on category order
     def reorder_list(self, local_only = False):
 
         self.grocery_list.sort(key=itemgetter(1))  # itemgetter tells python which key to use to sort on. in this case category_order.
         if local_only is not True:
             self.wp.reorder_list(self.wunderlist_list_id, [i[0] for i in self.grocery_list])
+
+    def get_next_new_ingredient(self):
+        new_ingredients = [e for e in self.grocery_list if e[1] == None]
+
+        if len(new_ingredients) > 0:
+            return new_ingredients[0]
+        else:
+            return None
--- main_ui_sort_list.py
+++ main_ui_sort_list.py
@@ -11,6 +11,7 @@ wp = wunderpy_wrapper.wunderpy_wrapper()
 
 grocery_store = grocerystore.groceryStore('./data/store_order_zehrs.csv')
 groceries = grocerylist.groceryList(wp.WUNDERLIST_GROCERY, wp)
+groceries.get_categories(grocery_store)
 
 # load the UI class objects
 UiMainWindow, QtBaseClass = uic.loadUiType('./ui/missing_ingredient.ui')
@@ -24,8 +25,13 @@ class MyApp(QtWidgets.QMainWindow, UiMainWindow):
 
         self.populate_categories()
 
-        self.QbuttonSelect.clicked.connect(self.select_category)
-        self.QbuttonSkip.clicked.connect(self.skip_category_select)
+        self.next_missing_ingredient()
+
+        if self.missing_ingredient is not None:
+            self.QbuttonSelect.clicked.connect(self.select_category_and_next)
+            self.QbuttonSkip.clicked.connect(self.skip_category_select)
+
+
 
     #populate QlistCategories from grocery store
     def populate_categories(self):
@@ -36,9 +42,45 @@ class MyApp(QtWidgets.QMainWindow, UiMainWindow):
             thisItem.setData(99,cat[0]) # read self.QlistCategories.item(<selected>).data(99)
             self.QlistCategories.addItem(thisItem)
 
+    def next_missing_ingredient(self):
+        new_ingred = groceries.get_next_new_ingredient()
+
+        if new_ingred == None:
+            self.missing_ingredient = None
+            groceries.reorder_list()
+            self.set_next_ingredient('List re-ordered.')
+
+            self.QbuttonSelect.setDisabled(True)
+            self.QbuttonSkip.setDisabled(True)
+
+        else:
+            self.set_next_ingredient(new_ingred[2])
+            self.missing_ingredient = new_ingred
+
+    # todo there is some repetition here that would be nice to fix
+    def select_category_and_next(self):
+        selected_id = self.get_selected_cagetory('id')
+
+        if self.missing_ingredient is not None and selected_id is not None:
+            self.QlastAction.setText('Added ' +self.missing_ingredient[2] + ' to ' + str(selected_id))
+
+            grocery_store.add_ingredient_to_category(self.missing_ingredient[2], selected_id)
+            groceries.set_category_for_item(self.missing_ingredient[2],selected_id)
+
+            self.next_missing_ingredient()
+
+    # todo there is some repetition here that would be nice to fix
+    def skip_category_select(self):
+        selected_id = self.get_selected_cagetory('id')
+
+        if self.missing_ingredient is not None and selected_id is not None:
+            self.QlastAction.setText('Set ' + self.missing_ingredient[2] + ' to ' + str(selected_id))
+
+            groceries.set_category_for_item(self.missing_ingredient[2], selected_id)
+            self.next_missing_ingredient()
 
     #on QbuttonSelect click get the selectedItem() from QlistCategories
-    def select_category(self):
+    def get_selected_cagetory(self, ret='id'):
 
         selectedItems = self.QlistCategories.selectedItems()
 
@@ -46,26 +88,32 @@ class MyApp(QtWidgets.QMainWindow, UiMainWindow):
             selected_category = selectedItems[0].text()
             selected_category_id = selectedItems[0].data(99)
         else:
-            # > 1 or 0 categories selected. we dont do that.
+            # > 1 or 0 categories selected. we don't do that.
             selected_category = None
+            selected_category_id = None
 
-        #self.QtextMissing_ingredient.setText(selected_category + "  " + str(selected_category_id))
+        # self.QtextMissing_ingredient.setText(selected_category + "  " + str(selected_category_id))
 
-    def skip_category_select(self):
-        return None
+        if ret == 'name':
+            return selected_category
+        else:
+            return selected_category_id
 
 
-    #def next_ingredient(self):
-    #groceries.get_tasks()
+    def set_next_ingredient(self, text):
+        self.QtextMissing_ingredient.setText(text)
 
     #groceries.get_categories(grocery_store)
     #groceries.reorder_list()
 
 # main program
 if __name__ == "__main__":
+
     app = QtWidgets.QApplication(sys.argv)
+
     window = MyApp()
     window.show()
+
     sys.exit(app.exec_())
 
 
